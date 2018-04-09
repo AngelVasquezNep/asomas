94eb168ed7c15d5fcb779eb1432bba653db3cde0
Ángel Vásquez <angel.vasquez.unam@gmail.com>
2018-04-09 17:11:02 -0500

    Trabajando en Galeria, agregando imagenes

--- pages/galeria.vue
+++ pages/galeria.vue
@@ -1,36 +1,52 @@
 <template lang="pug">
   #galeria
     .container
-      .item.level-1
+      //- 540-960-6   -
+      //- 720-248-1   -
+      //- 720-960-8   - 2-2
+      //- 960-548-10  - 3-1
+      //- 960-897-2   - 3-2
+      //- 960-720-3   - 3-2
+      //- 960-720-5   - 3-2
+      //- 960-720-7   - 3-2
+      //- 960-720-9   - 3-2
+
+      .item.column-2.row-1
         img(v-lazy="require('~/assets/img1.jpg')", alt='a')
-      .item.level-1
-        img(v-lazy="require('~/assets/img3.jpg')", alt='a')
-      .item.level-1
-        img(v-lazy="require('~/assets/img1.jpg')", alt='a')
-      .item.level-1
+      .item.column-3.row-2
         img(v-lazy="require('~/assets/img2.jpg')", alt='a')
-      .item.level-1
-        img(v-lazy="require('~/assets/img5.jpg')", alt='a')
-      .item.level-1
-        img(v-lazy="require('~/assets/img4.jpg')", alt='a')
-      .item.level-1
-        img(v-lazy="require('~/assets/logo.png')", alt='a')
-      .item.level-1
-        img(v-lazy="require('~/assets/img4.jpg')", alt='a')
-      .item.level-1
-        img(v-lazy="require('~/assets/logo.png')", alt='a')
-      .item.level-1
-        img(v-lazy="require('~/assets/img1.jpg')", alt='a')
-      .item.level-1
-        img(v-lazy="require('~/assets/img2.jpg')", alt='a')
-      .item.level-1
+      .item.column-3.row-2
         img(v-lazy="require('~/assets/img3.jpg')", alt='a')
-      .item.level-1
-        img(v-lazy="require('~/assets/img4.jpg')", alt='a')
-      .item.level-1
+      .item.column-3.row-2
         img(v-lazy="require('~/assets/img5.jpg')", alt='a')
-      .item.level-1
-        img(v-lazy="require('~/assets/logo.png')", alt='a')
+      .item.column-1.row-2
+        img(v-lazy="require('~/assets/img6.jpg')", alt='a')
+      .item.column-3.row-2
+        img(v-lazy="require('~/assets/img7.jpg')", alt='a')
+      .item.column-2.row-2
+        img(v-lazy="require('~/assets/img8.jpg')", alt='a')
+      .item.column-3.row-2
+        img(v-lazy="require('~/assets/img9.jpg')", alt='a')
+      .item.column-2.row-1
+        img(v-lazy="require('~/assets/img10.jpg')", alt='a')
+      .item.column-2
+        img(v-lazy="require('~/assets/img1.jpg')", alt='a')
+      .item.column-3.row-2
+        img(v-lazy="require('~/assets/img2.jpg')", alt='a')
+      .item.column-3.row-2
+        img(v-lazy="require('~/assets/img3.jpg')", alt='a')
+      .item.column-3.row-2
+        img(v-lazy="require('~/assets/img5.jpg')", alt='a')
+      .item.column-1.row-1
+        img(v-lazy="require('~/assets/img6.jpg')", alt='a')
+      .item.column-3.row-2
+        img(v-lazy="require('~/assets/img7.jpg')", alt='a')
+      .item.column-2.row-2
+        img(v-lazy="require('~/assets/img8.jpg')", alt='a')
+      .item.column-3.row-2
+        img(v-lazy="require('~/assets/img9.jpg')", alt='a')
+      .item.column-3.row-2
+        img(v-lazy="require('~/assets/img10.jpg')", alt='a')
 
 
 
@@ -67,35 +83,49 @@
 #galeria
   margin-bottom: 40px  
 .container
+  // align-items: center
   display: grid
-  grid-template-columns: repeat(4, 1fr)
-  grid-gap: 10px
+  grid-template-columns: repeat(6, 1fr)
+  grid-gap: 5px
   justify-content: center
   grid-auto-flow: dense
   @media screen and (max-width: 768px)
-    grid-template-columns: repeat(2, 1fr)
-  @media screen and (max-width: 542px)
-    grid-template-columns: 1fr
+    grid-template-columns: repeat(1, 1fr)
+    .column-2, .column-3, .column-4, .column-5, .column-6
+      grid-column-end: span 1
+    .row-2, .row-3, .row-4, .row-5, .row-6
+      grid-row-end: span 1
 
 .item
   border-radius: 5px
   padding: 10px
-  background-color: #ececec
+  background-color: #f4f4f4
   img
     max-width: 100%
 
-.level-1
+.column-1
   grid-column-end: span 1
 
-.level-2
+.column-2
   grid-column-end: span 2
 
-.level-3
+.column-3
   grid-column-end: span 3
 
-.level-4
+.column-4
   grid-column-end: span 4
 
-.level-5
+.column-5
   grid-column-end: span 5
+
+.row-1
+  grid-row-end: span 1
+
+.row-2
+  grid-row-end: span 2
+
+.row-3
+  grid-row-end: span 3
+
+
 </style>
